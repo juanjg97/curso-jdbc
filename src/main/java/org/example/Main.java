a0a943efe9b40cbc0190b9d17e4ad814da06dc46
juanjg97 <juanjosejg2711@gmail.com>
2023-07-26 23:06:03 -0600

    Se refactoriza el código poniendo los ejemplos dentro de funciones.
    Se prueba executeQuery

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,54 +12,95 @@ import java.util.Properties;
 import org.h2.tools.RunScript;
 
 public class Main {
-    public static void main(String[] args) {
-        try {
-            Properties properties = new Properties();
-            properties.setProperty("user", "sa");
-            properties.setProperty("password", "");
-            System.out.println("Connecting...");
+    public static void main(String[] args) throws SQLException, FileNotFoundException {
+        Connection h2Connection = createConnection();
 
-            Connection connection = DriverManager.getConnection("jdbc:h2:~/test",properties); // Establish the connection to the H2 database
-
-            System.out.println("Connected"); //Ejecución de un script (específico para h2)
-
-            System.out.println("Executing Script");
-            RunScript.execute(connection,new FileReader("src/main/resources/schema.sql"));
-            System.out.println("Script Excecuted");
-
-
-            PreparedStatement insertStatement = connection
-                    .prepareStatement("INSERT INTO employee (name,last_name,salary) VALUES (?,?,?)");
-
-            insertStatement.setString(1,"Juan");
-            insertStatement.setString(2,"José");
-            insertStatement.setDouble(3,123.45);
-            int rowsAffected = insertStatement.executeUpdate();
-            System.out.println("Rows affected: " + rowsAffected);
-
-            insertStatement.setString(1,"Carlos");
-            insertStatement.setString(2,"Alejandro");
-            insertStatement.setDouble(3,567.45);
-            rowsAffected = insertStatement.executeUpdate();
-            System.out.println("Rows affected: " + rowsAffected);
-
-            PreparedStatement deleteStatement = connection
-                    .prepareStatement("DELETE FROM employee");
-
-            int rowsDeleted = deleteStatement.executeUpdate();
-            System.out.println("Rows deleted: " + rowsDeleted);
-
-            deleteStatement.close();
-            insertStatement.close();
-
-            System.out.println("Closing...");
-            connection.close();
-            System.out.println("Closed");
-        } catch (SQLException e) {
-            // Handle any exceptions that occur during the connection process
-            e.printStackTrace();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+        //firstExample(h2Connection);
+        secondExample(h2Connection);
     }
+
+    static Connection createConnection() throws SQLException {
+        Properties properties = new Properties();
+        properties.setProperty("user", "sa");
+        properties.setProperty("password", "");
+        System.out.println("Connecting...");
+        return DriverManager.getConnection("jdbc:h2:~/test",properties);
+    }
+
+    static void firstExample(Connection h2Connection) throws SQLException, FileNotFoundException {
+
+        System.out.println("Connected"); //Ejecución de un script (específico para h2)
+        System.out.println("Executing Script");
+        RunScript.execute(h2Connection,new FileReader("src/main/resources/schema.sql"));
+        System.out.println("Script Excecuted");
+
+        //INSERT INTO
+        PreparedStatement insertStatement = h2Connection.prepareStatement("INSERT INTO employee (name,last_name,salary) VALUES (?,?,?)");
+
+        insertStatement.setString(1,"Juan");insertStatement.setString(2,"Jose");insertStatement.setDouble(3,123.45);
+        int rowsAffected = insertStatement.executeUpdate();
+        System.out.println("Rows affected: " + rowsAffected);
+
+        insertStatement.setString(1,"Carlos");insertStatement.setString(2,"Alejandro");insertStatement.setDouble(3,567.45);
+        rowsAffected = insertStatement.executeUpdate();
+        System.out.println("Rows affected: " + rowsAffected);
+
+        //DELETE FROM
+        PreparedStatement deleteStatement = h2Connection.prepareStatement("DELETE FROM employee");
+
+        int rowsDeleted = deleteStatement.executeUpdate();
+        System.out.println("Rows deleted: " + rowsDeleted);
+
+        deleteStatement.close();
+        insertStatement.close();
+
+        System.out.println("Closing...");
+        h2Connection.close();
+        System.out.println("Closed");
+    }
+
+    static void secondExample(Connection h2Connection) throws SQLException, FileNotFoundException {
+
+        System.out.println("Connected");System.out.println("Executing Script");
+        RunScript.execute(h2Connection,new FileReader("src/main/resources/schema.sql")); //Ejecución de un script (específico para h2)
+        System.out.println("Script Excecuted");
+
+        //INSERT INTO
+        PreparedStatement insertStatement = h2Connection.prepareStatement("INSERT INTO employee (name,last_name,salary) VALUES (?,?,?)");
+
+        insertStatement.setString(1,"Juan");insertStatement.setString(2,"Jose");insertStatement.setDouble(3,123.45);
+        int rowsAffected = insertStatement.executeUpdate();
+        System.out.println("Rows affected: " + rowsAffected);
+
+        insertStatement.setString(1,"Carlos");insertStatement.setString(2,"Alejandro");insertStatement.setDouble(3,567.45);
+        rowsAffected = insertStatement.executeUpdate();
+        System.out.println("Rows affected: " + rowsAffected);
+
+        insertStatement.close();
+
+        PreparedStatement selectStatement =  h2Connection.prepareStatement("SELECT * FROM employee");
+
+
+        ResultSet resultSet=selectStatement.executeQuery();
+        while (resultSet.next()){
+            System.out.printf("\nid: [%d] Name: [%s] LastName: [%s] Salary: [%f]",
+                    resultSet.getInt(1),
+                    resultSet.getString(2),
+                    resultSet.getString(3),
+                    resultSet.getDouble(4)
+            );
+        }
+
+        selectStatement.close();
+
+
+
+
+        System.out.println("Closing...");
+        h2Connection.close();
+        System.out.println("Closed");
+    }
+
+
+
 }

cc071ce560611463911e453755ed40ef991c109c
juanjg97 <juanjosejg2711@gmail.com>
2023-07-31 17:57:22 -0600

    Se completa la prueba de tiempo con las diferentes PoolConnections

--- src/main/java/datasources/ConnectiosTime.java
+++ src/main/java/datasources/ConnectiosTime.java
@@ -1,7 +1,9 @@
 package datasources;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.h2.jdbcx.JdbcConnectionPool;
 
 import java.sql.Connection;
@@ -14,11 +16,13 @@ public class ConnectiosTime {
 
     public static void main(String[] args) throws SQLException {
         ArrayList<Integer> numberOfConnections =
-                new ArrayList<>(Arrays.asList(1, 10, 20,30,100,200));
+                new ArrayList<>(Arrays.asList(1, 10, 20,30,100));
 
         tryConnectionsDm(numberOfConnections);
-        tryConnectionsPc(numberOfConnections);
-        tryConnectionsHikari(numberOfConnections);
+        tryConnectionsH2Pc(numberOfConnections);
+        tryConnectionsHikariPc(numberOfConnections);
+        tryConnectionsDbcP2Pc(numberOfConnections);
+        tryConnections3CPO(numberOfConnections);
     }
 
     static void tryConnectionsDm(ArrayList<Integer>numberOfConnections) throws SQLException {
@@ -36,8 +40,8 @@ public class ConnectiosTime {
         }
     }
 
-    static void tryConnectionsPc(ArrayList<Integer>numberOfConnections) throws SQLException {
-        System.out.println("============Time Using H2 Pool Connection");
+    static void tryConnectionsH2Pc(ArrayList<Integer>numberOfConnections) throws SQLException {
+        System.out.println("\n============Time Using H2 Pool Connection");
         for(Integer connections:numberOfConnections) {
             long startTime = System.currentTimeMillis();
             JdbcConnectionPool connectionPool =
@@ -53,8 +57,8 @@ public class ConnectiosTime {
         }
     }
 
-    static void tryConnectionsHikari(ArrayList<Integer>numberOfConnections) throws SQLException {
-        System.out.println("============Time Using Hikari Pool Connection");
+    static void tryConnectionsHikariPc(ArrayList<Integer>numberOfConnections) throws SQLException {
+        System.out.println("\n============Time Using Hikari Pool Connection");
         for(Integer connections:numberOfConnections) {
             long startTime = System.currentTimeMillis();
             HikariConfig config = new HikariConfig();
@@ -70,6 +74,50 @@ public class ConnectiosTime {
             }
             hikariDatasource.close();
 
+            System.out.println("Total time for: "
+                    +connections
+                    +" connections: "
+                    +(System.currentTimeMillis()-startTime)+"ms");
+        }
+    }
+
+    static void tryConnectionsDbcP2Pc(ArrayList<Integer>numberOfConnections) throws SQLException {
+        System.out.println("\n============Time Using DbcP2");
+        for(Integer connections:numberOfConnections) {
+            long startTime = System.currentTimeMillis();
+            BasicDataSource dbcpDatasource=new BasicDataSource();
+            dbcpDatasource.setUrl("jdbc:h2:~/test");
+            dbcpDatasource.setUsername("sa");
+            dbcpDatasource.setPassword("");
+
+            for (int i = 0; i <connections; i++) {
+                Connection connection =dbcpDatasource.getConnection();
+                connection.close();
+            }
+            dbcpDatasource.close();
+
+            System.out.println("Total time for: "
+                    +connections
+                    +" connections: "
+                    +(System.currentTimeMillis()-startTime)+"ms");
+        }
+    }
+
+    static void tryConnections3CPO(ArrayList<Integer>numberOfConnections) throws SQLException {
+        System.out.println("\n============Time Using 3CPO");
+        for(Integer connections:numberOfConnections) {
+            long startTime = System.currentTimeMillis();
+
+            ComboPooledDataSource c3po= new ComboPooledDataSource();
+            c3po.setJdbcUrl("jdbc:h2:~/test");
+            c3po.setUser("sa");
+            c3po.setPassword("");
+
+            for (int i = 0; i <connections; i++) {
+                Connection connection=c3po.getConnection();
+                connection.close();
+            }
+            c3po.close();
             System.out.println("Total time for: "
                     +connections
                     +" connections: "

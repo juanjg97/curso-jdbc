5ea33c0f7d2e87921d40fd49c71a3305e4dcbebb
juanjg97 <juanjosejg2711@gmail.com>
2023-07-31 17:00:51 -0600

    Se agrega prueba con Pool Connection

--- src/main/java/datasources/ConnectiosTime.java
+++ src/main/java/datasources/ConnectiosTime.java
@@ -1,5 +1,7 @@
 package datasources;
 
+import org.h2.jdbcx.JdbcConnectionPool;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -10,17 +12,36 @@ public class ConnectiosTime {
 
     public static void main(String[] args) throws SQLException {
         ArrayList<Integer> numberOfConnections =
-                new ArrayList<>(Arrays.asList(1, 10, 20,30,100,1000));
+                new ArrayList<>(Arrays.asList(1, 10, 20,30,100,500));
 
-        tryConnections(numberOfConnections);
+        tryConnectionsDm(numberOfConnections);
+        tryConnectionsPc(numberOfConnections);
     }
 
-    static void tryConnections(ArrayList<Integer>numberOfConnections) throws SQLException {
+    static void tryConnectionsDm(ArrayList<Integer>numberOfConnections) throws SQLException {
+        System.out.println("============Time Driver Manager Connection");
         for(Integer connections:numberOfConnections) {
             long startTime = System.currentTimeMillis();
             for (int i = 0; i <connections; i++) {
                 Connection h2Connection = DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
                 h2Connection.close();
+            }
+            System.out.println("Total time for: "
+                    +connections
+                    +" connections: "
+                    +(System.currentTimeMillis()-startTime)+"ms");
+        }
+    }
+
+    static void tryConnectionsPc(ArrayList<Integer>numberOfConnections) throws SQLException {
+        System.out.println("============Time Using Pool Connection");
+        for(Integer connections:numberOfConnections) {
+            long startTime = System.currentTimeMillis();
+            JdbcConnectionPool connectionPool =
+                    JdbcConnectionPool.create("jdbc:h2:~/test","sa","");
+            for (int i = 0; i <connections; i++) {
+                Connection connection = connectionPool.getConnection();
+                connection.close();
             }
             System.out.println("Total time for: "
                     +connections
